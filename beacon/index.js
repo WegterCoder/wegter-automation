29e6ff4aed477a5517644d24d3ef32ecac4c7d37
Wegter Coder <github@wegter.com>
2023-05-21 11:20:31 +0200

    Beacon enhancement

--- beacon/index.js
+++ beacon/index.js
@@ -2,9 +2,10 @@ module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
 
     const payload = (req.query || req.body );
-    const responseMessage = payload == {}
-        ? payload
-        : "Wegter Automation function executed successfully.";
+    const responseMessage = {
+        Message: "Wegter Automation function executed successfully.",
+        Request: req
+    }
 
     context.res = {
         // status: 200, /* Defaults to 200 */
