003e27b0c1f3b6bd6c176dafe1f1b66aae73f3aa
Wegter Coder <github@wegter.com>
2023-06-05 19:12:38 +0200

    Beacon with exceptions tracing

--- beacon/index.js
+++ beacon/index.js
@@ -1,14 +1,33 @@
 module.exports = async function (context, req) {
-    context.log('JavaScript HTTP trigger function processed a request.');
 
-    const payload = (req.query || req.body );
-    const responseMessage = {
-        Message: "Wegter Automation function executed successfully.",
-        Request: req
+    let responseMessage = {status: 200, body:null};
+    let exceptionRecords = [];
+    /* Exceptions handling */
+    try {
+
+        if (req.query.test == 'exception') {req.nonExistingFunction()}
+        
+
+        context.bindings.events = [];
+        const payload = (req.query || req.body );
+        responseMessage.body = {
+            TimeStamp: new Date().toISOString(),
+            Message: "Wegter Automation function executed successfully.",
+            BindingData: context.bindingData
+        }
+    } catch (error) {
+        exceptionRecords.push({name:error.name,message:error.message,stack:error.stack});
+        responseMessage = {status: 500, body:`Logged event: AF-${context.bindingData.sys.methodName} "${context.executionContext.invocationId}"`}
     }
 
-    context.res = {
-        // status: 200, /* Defaults to 200 */
-        body: responseMessage
-    };
+    // http out binding
+    context.res = responseMessage;
+
+    // table out binding
+    context.bindings.event = {
+        PartitionKey: `AF-${context.bindingData.sys.methodName}`,
+        RowKey: JSON.stringify(context.executionContext.invocationId),
+        JSON: JSON.stringify(context.bindingData),
+        JSONExceptions: exceptionRecords
+    }
 }
